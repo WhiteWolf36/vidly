107bff83a79def6d100dd928417b907584b69cf6
WhiteWolf36 <ahmadking78654@gmail.com>
2023-09-18 00:36:17 +0500

    Integration testing of auth middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const { User } = require("../../models/user");
+const { Genere } = require("../../models/genere");
+
+describe("auth middleware", () => {
+  let server;
+  beforeEach(() => {
+    server = require("../../index");
+  });
+  afterEach(async () => {
+    server.close();
+    await Genere.deleteMany({});
+  });
+  let token;
+  const exec = () => {
+    return request(server)
+      .post("/api/generes")
+      .set("x-auth-token", token)
+      .send({ name: "genere1" });
+  };
+
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+  });
+
+  it("should return 401 if no token is provided ", async () => {
+    token = "";
+    const response = await exec();
+    expect(response.status).toBe(401);
+  });
+
+  it("should return 400 if invalid token is provided ", async () => {
+    token = "dsd";
+    const response = await exec();
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 200 if valid token is provided ", async () => {
+    const response = await exec();
+    expect(response.status).toBe(200);
+  });
+});
