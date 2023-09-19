dd495104b5bfd6be658b8d8063664c29a2157270
WhiteWolf36 <ahmadking78654@gmail.com>
2023-09-20 04:14:57 +0500

    mongoose static methods

--- models/rental.js
+++ models/rental.js
@@ -1,61 +1,66 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
+const rentalSchema = mongoose.Schema({
+  customer: {
+    type: mongoose.Schema({
+      name: {
+        type: String,
+        minLength: 5,
+        maxLength: 50,
+        required: true,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: Number,
+        required: true,
+        min: 5,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLEngth: 255,
+        trim: true,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
 
-const Rental = mongoose.model(
-  "Rental",
-  mongoose.Schema({
-    customer: {
-      type: mongoose.Schema({
-        name: {
-          type: String,
-          minLength: 5,
-          maxLength: 50,
-          required: true,
-        },
-        isGold: {
-          type: Boolean,
-          default: false,
-        },
-        phone: {
-          type: Number,
-          required: true,
-          min: 5,
-        },
-      }),
-      required: true,
-    },
-    movie: {
-      type: mongoose.Schema({
-        title: {
-          type: String,
-          required: true,
-          minLength: 5,
-          maxLEngth: 255,
-          trim: true,
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 255,
-        },
-      }),
-      required: true,
-    },
-    dateOut: {
-      type: Date,
-      required: true,
-      default: Date.now,
-    },
-    dateReturned: {
-      type: Date,
-    },
-    rentalFee: {
-      type: Number,
-      min: 0,
-    },
-  })
-);
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    "customer._id": customerId,
+    "movie._id": movieId,
+  });
+};
+
+const Rental = mongoose.model("Rental", rentalSchema);
 
 function validateRental(rental) {
   const schema = Joi.object({
--- routes/returns.js
+++ routes/returns.js
@@ -3,19 +3,13 @@ const { Rental } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const auth = require("../middleware/auth");
 const moment = require("moment");
+const Joi = require("joi");
+const validate = require("../middleware/validate");
 
 const router = express.Router();
-router.post("/", auth, async (req, res) => {
-  if (!req.body.customerId) {
-    return res.status(400).send("No customer Id found!");
-  }
-  if (!req.body.movieId) {
-    return res.status(400).send("No customer Id found!");
-  }
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+router.post("/", [auth, validate(validateReturn)], async (req, res) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+
   if (!rental) return res.status(404).send("No rental found!");
   if (rental.dateReturned) {
     return res.status(400).send("Rental is already proccessed");
@@ -34,5 +28,12 @@ router.post("/", auth, async (req, res) => {
   await rental.save();
   return res.status(200).send(rental);
 });
+function validateReturn(req) {
+  const schema = Joi.object({
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  });
+  return schema.validate(req);
+}
 
 module.exports = router;
