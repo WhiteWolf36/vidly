ef3d4ca8ab00e91d0d41f36b80045fa61e8e9a6f
WhiteWolf36 <ahmadking78654@gmail.com>
2023-09-20 05:33:34 +0500

    final

--- startup/logging.js
+++ startup/logging.js
@@ -2,6 +2,7 @@ const winston = require("winston");
 require("winston-mongodb");
 require("express-async-errors");
 module.exports = function () {
+  const db = process.env.MONGODB_URI;
   winston.exceptions.handle(
     new winston.transports.Console(),
     new winston.transports.File({ filename: "unhandledExceptions.log" })
@@ -11,7 +12,5 @@ module.exports = function () {
   });
 
   winston.add(new winston.transports.File({ filename: "logfile.log" }));
-  winston.add(
-    new winston.transports.MongoDB({ db: "mongodb://127.0.0.1:27017/vidly" })
-  );
+  winston.add(new winston.transports.MongoDB({ db }));
 };
